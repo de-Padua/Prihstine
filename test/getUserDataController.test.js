0543e31fee1c3a0f2630c8c2f673dc26d8f07b5e
Antonio <toninhoport@gmail.com>
2024-06-25 18:00:13 -0300

    add : added the mock for controllers test

--- test/getUserDataController.test.js
+++ test/getUserDataController.test.js
@@ -1,26 +1,15 @@
 
-
-// Assuming you are using Jest for testing
-
 const controller = require("../controllers/user_controller");
 
-// Mocks for req and res objects
 const MockReq = {};
 const MockRes = {
   json: jest.fn(),
-  status: jest.fn(() => MockRes), // Mock status chaining
+  status: jest.fn(() => MockRes),
 };
-
 describe('Test getUserData route', () => {
   test('Should return user data', async () => {
-    await controller.getUserData(MockReq, MockRes);
-
-    // Check if res.json was called with the expected userData
-    expect(MockRes.json).toHaveBeenCalledWith({ data: "ok" });
-
-    // Check that status was not called (indicating no error occurred)
+    await controller.getUserData(MockReq, MockRes); 
+    expect(MockRes.json).toHaveBeenCalledWith({ data: "" });  // <<< this will fail
     expect(MockRes.status).not.toHaveBeenCalled();
   });
-
-  // You can add more tests here to cover other scenarios if needed
 });
