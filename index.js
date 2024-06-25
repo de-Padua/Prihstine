fe9d15a8fd51b4140c149a51bacdae85ad2d4412
Antonio <toninhoport@gmail.com>
2024-06-25 05:59:09 -0300

    Feat : create basic server with express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("2")
+const express = require("express");
+const app = express();
+
+const port = 5678;
+
+// Define a route handler for the root endpoint "/"
+app.get("/", (req, res) => {
+    res.json({ data: "oi" }); // Return JSON response
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
