4624080a20ed53548143217214f45db09b6fbd52
Antonio <toninhoport@gmail.com>
2024-06-25 16:10:44 -0300

    add: added the user routes

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
-
+const userRoutes = require("./routes/user")
 const port = 5678;
 
-// Define a route handler for the root endpoint "/"
-app.get("/", (req, res) => {
-    res.json({ data: "oi" }); // Return JSON response
-});
+app.use(userRoutes)
 
 // Start the server
 app.listen(port, () => {
