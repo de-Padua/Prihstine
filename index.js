bc29dd0f40569083a2af42f12cfadbcddf3393dc
Antonio <toninhoport@gmail.com>
2024-07-02 10:40:22 -0300

    add : added prisma middleware

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/user")
+const prisma = require("./db/db")
 const port = 5678;
+
 
 app.use(userRoutes)
 
+app.use((req, res, next) => {
+  req.prisma = prisma;
+  next();
+});
+
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
+
