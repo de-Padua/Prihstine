2b8e6f832d7bae386b9e5df56e05fc60160cb87c
Antonio <toninhoport@gmail.com>
2024-07-04 18:37:48 -0300

    feat: Add raw body parsing middleware for handling POST, PUT, DELETE requests

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const express = require("express");
 const app = express();
-const userRoutes = require("./routes/user")
-const prisma = require("./db/db")
+const getRawBody = require("raw-body");
+const contentType = require('content-type');
+const userRoutes = require("./routes/user");
+const prisma = require("./db/db");
 const port = 5678;
 
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
-app.use(userRoutes)
+// Middleware to process raw body for POST, PUT, DELETE requests
+app.use((req, res, next) => {
+  if (!['POST', 'PUT', 'DELETE'].includes(req.method)) {
+    return next();
+  }
+  getRawBody(req, {
+    length: req.headers['content-length'],
+    limit: '1kb',
+    encoding: contentType.parse(req).parameters.charset || 'utf-8'
+  }, (err, string) => {
+    if (err) return next(err);
+    req.text = string;
+    next();
+  });
+});
 
+// Add Prisma to request
 app.use((req, res, next) => {
   req.prisma = prisma;
   next();
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// Use routes
+app.use(userRoutes);
 
+// Start the server
