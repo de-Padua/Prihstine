978a2d887c5d2227bd98a38465bef39e82db1648
Antonio <toninhoport@gmail.com>
2024-07-06 19:18:21 -0300

    test

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,11 +3,15 @@ const logger = require("../helpers/logger");
 const _db = require("../db/db");
 const bodyValidation = require("../helpers/bodyValidation");
 var bcrypt = require("bcryptjs");
-const { ca } = require("date-fns/locale");
+const validateSession = require("../helpers/checkUserSession");
+const getUserById = require("../helpers/getUserById");
 
 const userController = {
   createNewUser: async (req, res) => {
     try {
+
+      console.log("oi")
+
       const jsonBody = req.body;
 
       const validation = headerValidation(req.headers);
@@ -68,44 +72,37 @@ const userController = {
     } catch (error) {
       logger({ error: error });
 
-      return res.status(500).json({ error }).end();
+      return res.status(500).json(error).end();
     }
   },
 
   getUser: async (req, res) => {
-    try {
-      const { userId } = req.params;
-
-      logger({ data: "request to get user field" });
-      const sensitiveUserFields = ["email", "password"];
-
-      const targetUser = await _db.user.findUnique({
-        where: { id: userId },
-        include: {
-          posts: true
-        },
-      });
-
-      if (!targetUser) {
-        logger({ data: "user not found" });
-        res.status(404);
-        
-      }
-
-      const nonSensitiveUserData = Object.fromEntries(
-        Object.entries(targetUser).filter(
-          (item) => !sensitiveUserFields.includes(item[0])
-        )
-      );
-      logger({ data: "filtered user field to non-sensitive data" });
-      res.json(nonSensitiveUserData).status(200);
-
-      logger({ data: "responded succesfuly" });
-    } catch (error) {
-      res.json(error).status(500);
-      logger({ data: error.name });
-    }
+  const users =  _db.user.findMany()
+  res.json(users)
   },
+  getCurrentUserSession : (req,res) =>{
+     const token = req.cookie["sid"]  
+
+     if(!token){
+      logger({data:"invalid user token,session invalid"})
+      res.status(404).json({data:"invalid data"})
+     }
+     const session = validateSession(token)
+
+     if(!session){
+      logger({data:"invalid session"})
+      res.status(401).json({data:"invalid user session,login again"})
+     }
+     
+    const user = getUserById(session.userId)
+
+    if(!user){
+      logger({data:"user doesn't exist"})
+      res.status(401).json({data:"user doesn't exist"})
+    }
+    res.status(206).json({data:user})
+
+  }
 };
 
 module.exports = userController;
