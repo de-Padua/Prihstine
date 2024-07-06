038e7ee5d39ecd51639ae08252514eb7f83e4a81
Antonio <toninhoport@gmail.com>
2024-07-06 19:20:26 -0300

    feat : add new route for session controller

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const route = app.Router()
 const UserController = require("../controllers/user_controller")
 
 route.post("/user", UserController.createNewUser)
-route.get("/user", UserController.getUser)
+route.get("/user/:userId}", UserController.getUser)
+route.get("/user/session}", UserController.getCurrentUserSession)
 
 module.exports = route
