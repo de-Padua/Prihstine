1bc11915ee0887c86fbb323bd8524dfb05148d52
Antonio <toninhoport@gmail.com>
2024-07-10 17:52:54 -0300

    feat : add headerValidation middleware to verify header requests data

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express();
 const userRoutes = require("./routes/user");
 const prisma = require("./db/db");
 const cookieParser = require('cookie-parser');
+const headerValidation = require("./helpers/headerValitation")
 const cors = require("cors");
 const port = 5678;
 
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json({ limit: "1mb" })); // Adjusted limit to 1MB
+app.use(express.json({ limit: "1kb" })); // Adjusted limit to 1MB
+app.use((req,res,next)=>{
+  headerValidation(req.headers,res)
+  next()
+})
 
 // Middleware to attach prisma to the request object
 app.use((req, res, next) => {
