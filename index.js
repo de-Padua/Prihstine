8261ea03e296c8a35d29f17b2eccb8cad1d85ccf
Antonio <toninhoport@gmail.com>
2024-07-10 17:59:07 -0300

    fix : refactor middleware to verify request methods and allow it through the headerValidation

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const express = require("express");
 const app = express();
 const userRoutes = require("./routes/user");
 const prisma = require("./db/db");
-const cookieParser = require('cookie-parser');
-const headerValidation = require("./helpers/headerValitation")
+const cookieParser = require("cookie-parser");
+const headerValidation = require("./helpers/headerValitation");
 const cors = require("cors");
 const port = 5678;
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json({ limit: "1kb" })); // Adjusted limit to 1MB
-app.use((req,res,next)=>{
-  headerValidation(req.headers,res)
-  next()
-})
+app.use((req, res, next) => {
+  if (req.method !== "POST") {
+   return next();
+  }
+  headerValidation(req.headers, res);
+  next();
+});
 
 // Middleware to attach prisma to the request object
 app.use((req, res, next) => {
@@ -26,4 +29,3 @@ app.use(userRoutes);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
