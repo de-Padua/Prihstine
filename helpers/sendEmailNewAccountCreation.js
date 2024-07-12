30395a2ed2e2273d4f127c0ec4fa5c98fd604ae8
Antonio <toninhoport@gmail.com>
2024-07-12 14:37:38 -0300

    feat : add email sender

--- helpers/sendEmailNewAccountCreation.js
+++ helpers/sendEmailNewAccountCreation.js
@@ -2,32 +2,62 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 const fs = require("fs");
 const path = require("path");
-const ejs  = require("ejs")
+const ejs = require("ejs");
 
+const emailHandler = async (
+  action,
+  mainTitle,
+  emailTitle,
+  url,
+  secundaryTitle,
+  buttonText
+) => {
+  if (action === "newUser") {
+    const emailPath = path.join(__dirname, "../views/newAccountVerify.ejs");
+    const template = await ejs.renderFile(emailPath, {
+      mainTitle: mainTitle,
+      url: url,
+      emailTitle:emailTitle,
+      buttonText:"Verify my account",
+      secundaryTitle: secundaryTitle,
+      buttonText: buttonText,
+    });
+    return template;
+  }
+  else if (action === "email verified") {
+    const emailPath = path.join(__dirname, "../views/newAccountVerifed.ejs");
+    const template = await ejs.renderFile(emailPath, {
+      mainTitle: mainTitle,
+      url: url,
+      emailTitle:emailTitle,
+      buttonText:"Go to site",
+      secundaryTitle: secundaryTitle,
+      buttonText: buttonText,
+    });
+    return template;
+  }
+};
 
-const emailHandler = async (mainMessage,postUrl,secundaryTitle,buttonText) => {
-  const emailPath = path.join(__dirname, "../views/email.ejs");
-  const template = await ejs.renderFile(emailPath,{mainMessage:mainMessage,postUrl:postUrl,secundaryTitle:secundaryTitle,buttonText:buttonText})
-  return template
-}
-
-
-const sendMail = async (data) => {
-
-   
-  const email = await emailHandler(data.mainTitle,data.url,data.secundaryTitle,data.buttonText)
+const sendMail = async (action, data) => {
+  const email = await emailHandler(action,
+    data.mainTitle,
+    data.emailTitle,
+    data.url,
+    data.secundaryTitle,
+    data.buttonText
+  );
 
   const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // Use `true` for port 465, `false` for all other ports
-
+    service: "gmail",
+    host: "smtp.gmail.email",
+    port: 465,
+    secure: true, // Use `true` for port 465, `false` for all other ports
     auth: {
-      user: process.env.EMAIL_DEV,
-      pass: process.env.EMAIL_PASSWORD_DEV,
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
- 
+
   // async..await is not allowed in global scope, must use a wrapper
   async function main() {
     // send mail with defined transport object
@@ -35,7 +65,7 @@ const sendMail = async (data) => {
       from: `"Trace team " ${process.env.EMAIL}`, // sender address
       to: data.emailToSend[0], // list of receivers
       subject: data.emailTitle, // Subject line
-      text:data.mainTitle , // plain text body
+      text: data.mainTitle, // plain text body
       html: email, // html body
     });
 
