7bda9edb819d2ccbb9734f93fe931dff7df0f78f
Antonio <toninhoport@gmail.com>
2024-07-16 13:03:55 -0300

    fix : refactor routes to make it more easy to read

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,35 @@
-const app = require("express")
-const route = app.Router()
-const UserController = require("../controllers/user_controller")
+const app = require("express");
+const route = app.Router();
+const UserController = require("../controllers/user_controller");
 
-route.post("/user", UserController.createNewUser)
-route.get("/user/:userId", UserController.getUser)
-route.get("/user/session/validate", UserController.getCurrentUserSession)
-route.get("/user/:userId/verify-email/:tokenId", UserController.verifyEmail)
+//create user
+route.post("/user", UserController.createNewUser);
+route.post(
+  "/user/:userId/change-password/:token",
+  UserController.changeUserPassword
+);
+//get user based on id
+route.get("/user/:userId", UserController.getUser);
+
+//validates recovery password session
+route.get(
+  "/user/:userId/validate-session/:token",
+  UserController.validateRecoveryPasswordSession
+);
+
+//validates user session based on cookie
+route.get("/user/session/validate", UserController.getCurrentUserSession);
+
+//validates user email
+route.get("/user/:userId/verify-email/:tokenId", UserController.verifyEmail);
+
+//create session for password recovery
+route.get(
+  "/user/:userId/recovery-password",
+  UserController.createOrUpdatePasswordRecoverySession
+);
+
+//change user password
 
 
-module.exports = route
+module.exports = route;
