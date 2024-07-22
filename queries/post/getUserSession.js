9a2fb22a607637bb19b1f8e08b1360c211b60cbc
Antonio <toninhoport@gmail.com>
2024-07-22 15:28:30 -0300

    feat : add new queries for post controller

---
+++ queries/post/getUserSession.js
@@ -0,0 +1,16 @@
+const prisma = require("../../db/db")
+
+
+const getUserSession = async (userToken) => {
+
+  const findUserSession = await prisma.session.findFirst({
+    where: {
+      sessionId: userToken
+    }
+  })
+
+  return findUserSession
+}
+
+
+module.exports = getUserSession
