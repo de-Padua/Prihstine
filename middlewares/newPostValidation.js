0089bb59becb697b0772775fc713828881f93974
Antonio <toninhoport@gmail.com>
2024-07-27 06:41:06 -0300

    fix : edit new post validation schema

--- middlewares/newPostValidation.js
+++ middlewares/newPostValidation.js
@@ -3,20 +3,25 @@ const yup = require("yup")
 
 const newPostValidation = async (req, res, next) => {
   try {
-    const body = req.body
     const postSchema = yup.object({
       postTitle: yup.string().max(200).required(),
-      postDescrition: yup.string().max(800).required(),
-      postValue: yup.number().max(200).required(),
-      address: yup.string().max(100).required(),
-      state: yup.string().max(100).required(),
-      cep: yup.string().max(100).required(),
+      postDescription: yup.string().max(1200).required(), // Fixed typo here
+      postValue: yup.number().max(20000000000).required(), // Ensure this matches the Int type in Prisma
+      address: yup.object({
+        cep: yup.string().required(), // Mark as required if necessary
+        logradouro: yup.string().optional(),
+        complemento: yup.string().optional(),
+        unidade: yup.string().optional(),
+        bairro: yup.string().required(),
+        localidade: yup.string().optional(),
+        uf: yup.string().optional(),
+        ibge: yup.string().optional(),
+        ddd: yup.string().optional(),
+      }),
       sizeInSquareMeters: yup.string().max(100).required(),
-      isClosed: yup.bool().required(),
-
+     
     });
-
-    await postSchema.validate(body,  { strict: true })
+    await postSchema.validate(req.body,  { strict: true })
     next()
 
   } catch (err) {
