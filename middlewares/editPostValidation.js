dce67d51e9b528b1f51be86b7eb698c014b348ea
Antonio <toninhoport@gmail.com>
2024-07-27 06:40:33 -0300

    fix : refactor post validation schema

--- middlewares/editPostValidation.js
+++ middlewares/editPostValidation.js
@@ -6,14 +6,21 @@ const editPostMiddleware = async (req, res, next) => {
     const body = req.body
     const postSchema = yup.object({
       postTitle: yup.string().max(200).optional(),
-      postDescrition: yup.string().max(800).optional(),
-      postValue: yup.number().max(200000000000000).optional(),
-      address: yup.string().max(100).optional(),
-      state: yup.string().max(100).optional(),
-      cep: yup.string().max(100).optional(),
+      postDescription: yup.string().max(1200).optional(), // Fixed typo here
+      postValue: yup.number().max(20000000000).optional(), // Ensure this matches the Int type in Prisma
+      address: yup.object({
+        cep: yup.string().optional(), // Mark as optional if necessary
+        logradouro: yup.string().optional(),
+        complemento: yup.string().optional(),
+        unidade: yup.string().optional(),
+        bairro: yup.string().optional(),
+        localidade: yup.string().optional(),
+        uf: yup.string().optional(),
+        ibge: yup.string().optional(),
+        ddd: yup.string().optional(),
+      }),
       sizeInSquareMeters: yup.string().max(100).optional(),
-      isClosed: yup.bool().optional(),
-
+     
     }).noUnknown(true)
 
     await postSchema.validate(body,  { strict: true })
